722591d6f0c8573364d26b25a663ae9b868585c9
kamatala ashish <kamatalaashish@MacBook-Pro.local>
2023-08-16 19:50:16 +0530

    Added server and client

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express", cors = require('cors'));
+
+// Initialize Express
+const app = express();
+
+app.use(cors({
+    origin: 'http://173.249.15.117:9128',
+}))
+
+// Create GET request
+app.get("/currentsong", async (req, res, body) => {
+    const fetch = (...args) => import('node-fetch')
+  .then(({default: fetch}) => fetch(...args));
+    try {
+        const apiResponse = await fetch(
+            'http://173.249.15.117:9128/currentsong?sid=1')
+        const apiResponseJson = await apiResponse.text()
+
+        res.send(apiResponseJson)
+      } catch (err) {
+        console.log(err)
+        res.status(500).send('Something went wrong')
+      }
+});
+
+// Initialize server
+app.listen(5005, () => {
+  console.log("Running on port 5005.");
+});
+
+module.exports = app;
